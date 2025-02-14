cbb59cfe2f9c266772adf01f8d170021e1039e64
Xorion99 <lcaracci99@libero.it>
2025-02-14 15:38:25 +0100

    primo commit

---
+++ BeachManageProject/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace BeachManage.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
